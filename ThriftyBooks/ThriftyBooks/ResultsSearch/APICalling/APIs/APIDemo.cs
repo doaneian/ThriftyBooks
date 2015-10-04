a17791636e2027bbd726b53ce246e565ddbe8940
doaneian <ian.doane@gmail.com>
2015-10-04 09:43:22 -0500

    - Another attempt to fix port
    - Set up general file structure

---
+++ ThriftyBooks/ThriftyBooks/ResultsSearch/APICalling/APIs/APIDemo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThriftyBooks.ResultsSearch.APICalling.APIs
+{
+    public class APIDemo
+    {
+    }
+}
---
+++ ThriftyBooks/ThriftyBooks/ResultsSearch/Search.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThriftyBooks.ResultsSearch
+{
+    public class Search
+    {
+    }
+}
