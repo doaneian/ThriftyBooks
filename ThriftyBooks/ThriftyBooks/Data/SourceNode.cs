041b169bf7b70b64c8543e3af9aed6ce7eecde9b
doaneian <ian.doane@gmail.com>
2015-10-05 09:49:13 -0500

    - Implemented linked list for storing search results

---
+++ ThriftyBooks/ThriftyBooks/Data/SourceNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThriftyBooks.Data
+{
+    public class SourceNode
+    {
+        SourceNode next;
+
+        String sourceName;
+        double price;
+        String link;
+
+        public SourceNode(String sourceName, double price, String link)
+        {
+            this.sourceName = sourceName;
+            this.price = price;
+            this.link = link;
+        }
+
+        public SourceNode getNext()
+        {
+            return next;
+        }
+
+        public String getSourceName()
+        {
+            return sourceName;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public String getLink()
+        {
+            return link;
+        }
+
+        public void setNext(SourceNode next)
+        {
+            this.next = next;
+        }
+
+        public void setSourceName(String sourceName)
+        {
+            this.sourceName = sourceName;
+        }
+
+        public void setPrice(double price)
+        {
+            this.price = price;
+        }
+
+        public void setLink(String link)
+        {
+            this.link = link;
+        }
+    }
+}
